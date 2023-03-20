5aa59e12c26ef3c961be27c09cf0fe1de9b63634
github_username <you@domain.com>
2023-03-21 00:43:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const names =['ali', 'rahmot', 'sujon', 'ashiq'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+      names.map(name => <li>{name}</li>)
+      }
+     <Persone name="Rahmot"></Persone>
+     <Friends nam="Ashiq" pross="student"></Friends>
+     <Friends pross="student" nam="Ashiq"></Friends>
     </div>
   );
+  
+}
+function Persone(props) {
+  console.log(props)
+ return <h2>Md Alhaj Sarkar & {props.name}</h2>
+}
+
+function Friends(props){
+  console.log(props)
+  return(
+    <div className="contaner">
+      <h2>My name is  {props.nam}</h2>
+      <p>My profetion {props.pross}</p>
+    </div>
+  )
 }
 
 export default App;
