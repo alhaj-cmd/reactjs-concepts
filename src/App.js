1bce977795a33ababa9dcd96435f1263fecacbb5
github_username <you@domain.com>
2023-03-21 15:22:39 +0600

    set count

--- src/App.js
+++ src/App.js
@@ -1,47 +1,29 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
-  const names =['ali', 'rahmot', 'sujon', 'ashiq'];
-  const products =[
-    {name:'sakil', roll:10, pro:'student'},
-    {name:'rahmot', roll:1, pro:'student'},
-    {name:'ashiq', roll:2, pro:'student'},
-  ]
+
   return (
     <div className="App">
-      {
-      names.map(name => <li>{name}</li>)
-      }
-      {
-      products.map(product => <Persone name={product.name} roll={product.roll}></Persone>)
-      }
-     
-     <Friends nam="Ashiq" pross="student"></Friends>
-     <Friends pross="student" nam="Ashiq"></Friends>
+    
+      <Counter></Counter>
+    
     </div>
   );
   
 }
-function Persone(props) {
-  console.log(props)
- return (
-  <div className="container">
-   <h2>My name is  {props.name}</h2>
-      <p>My profetion {props.roll}</p>
-  </div>
- )
-}
 
-function Friends(props){
-  console.log(props)
+function Counter(){
+  const [count , setCount] = useState(33);
   return(
-    <div className="contaner">
-      <h2>My name is  {props.nam}</h2>
-      <p>My profetion {props.pross}</p>
+    <div>
+      <h2>Counter: {count}</h2>
+      <button>Increment</button>
     </div>
   )
 }
+
 
 export default App;
